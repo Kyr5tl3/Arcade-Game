87df61aacc432b54170fc3cb88a9c3c30f65c16c
Kyrstle Breslin <kyrstle@live.com.au>
2018-05-11 21:22:51 +0100

    Updated enemy's position function

--- js/app.js
+++ js/app.js
@@ -16,6 +16,7 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += speed*dt
 };
 
 // Draw the enemy on the screen, required method for game
