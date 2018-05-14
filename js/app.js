85a607870f630ab39a2805a8d85b178bc3239712
Kyrstle Breslin <kyrstle@live.com.au>
2018-05-14 19:29:27 +0100

    Beautified

--- js/app.js
+++ js/app.js
@@ -7,32 +7,33 @@ var totalPoints = 0;
 var highscore = 0;
 
 // Enemies our player must avoid
-var Enemy = function(x , y) {
-    this.x = x;
-    this.y = y;
-    this.speed = Math.random()*301|0;
-    this.sprite = 'images/enemy-bug.png';
+var Enemy = function(x, y) {
+  this.x = x;
+  this.y = y;
+  this.speed = Math.random() * 301 | 0;
+  this.sprite = 'images/enemy-bug.png';
 };
 
 // Update the enemy's position
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-    if(this.x < 505){
-      this.x += this.speed * dt;
-    }
-  else{this.x = -50;}
+  // You should multiply any movement by the dt parameter
+  // which will ensure the game runs at the same speed for
+  // all computers.
+  if (this.x < 505) {
+    this.x += this.speed * dt;
+  } else {
+    this.x = -50;
+  }
 };
 
 // Draw the enemy on the screen
 Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 // Player class
-var Player = function (x , y){
+var Player = function(x, y) {
   this.x = x;
   this.y = y;
   this.sprite = 'images/char-cat-girl.png';
@@ -41,7 +42,7 @@ var Player = function (x , y){
 //Player update function
 Player.prototype.update = function(dt) {
   var thePlayer = this;
-  if(this.y == -50){
+  if (this.y == -50) {
     points += 100;
     this.points();
     this.restart();
@@ -49,114 +50,123 @@ Player.prototype.update = function(dt) {
     createGems();
   }
 
-  allEnemies.forEach(function(enemy){
+  allEnemies.forEach(function(enemy) {
     var xPosition = Math.abs(player.x - enemy.x);
     var yPosition = Math.abs(player.y - enemy.y);
-    if(xPosition<=50 && yPosition<=25){
-        thePlayer.restart();
-        if($('ul.lives li').length > 1){
-          $('ul.lives li:last-child').remove();
-        }
-        else{$('ul.lives li:last-child').remove();
-        player.gameOver();}
-    }});
+    if (xPosition <= 50 && yPosition <= 25) {
+      thePlayer.restart();
+      if ($('ul.lives li').length > 1) {
+        $('ul.lives li:last-child').remove();
+      } else {
+        $('ul.lives li:last-child').remove();
+        player.gameOver();
+      }
+    }
+  });
 
-    allGems.forEach(function(gem){
-      var xPosition = Math.abs(player.x - gem.x + 25);
-      var yPosition = Math.abs(player.y - gem.y + 75);
-      if(xPosition<=5 && yPosition<=5){
-        var thisGem = allGems.indexOf(gem);
-        allGems.splice(thisGem,1);
-        gem.points();
-        player.points();
-        }
-      });
+  allGems.forEach(function(gem) {
+    var xPosition = Math.abs(player.x - gem.x + 25);
+    var yPosition = Math.abs(player.y - gem.y + 75);
+    if (xPosition <= 5 && yPosition <= 5) {
+      var thisGem = allGems.indexOf(gem);
+      allGems.splice(thisGem, 1);
+      gem.points();
+      player.points();
+    }
+  });
 };
 
 //Player render function - renders character on screen
 Player.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 //Player handling function - using keys to navingate the players
-Player.prototype.handleInput = function(keyPressed){
+Player.prototype.handleInput = function(keyPressed) {
 
-  if(keyPressed == 'up' && 100 > this.y <= 400){
+  if (keyPressed == 'up' && 100 > this.y <= 400) {
     this.y -= 50;
   }
-  if(keyPressed == 'down' && this.y < 400){
+  if (keyPressed == 'down' && this.y < 400) {
     this.y += 50;
   }
-  if(keyPressed == 'left' && this.x >= 50){
+  if (keyPressed == 'left' && this.x >= 50) {
     this.x -= 50;
   }
-  if(keyPressed == 'right' && this.x < 400){
+  if (keyPressed == 'right' && this.x < 400) {
     this.x += 50;
   }
 };
 
 //Gems
-var Gem = function(x , y, img) {
-    this.x = x;
-    this.y = y;
-    this.sprite = img;
+var Gem = function(x, y, img) {
+  this.x = x;
+  this.y = y;
+  this.sprite = img;
 };
 
 // Draw gem on the screen
 Gem.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y, 50, 80);
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y, 50, 80);
 };
 
 //new Gem
 var allGems = [];
 
-function newGem(){
-  var calcX = Math.abs((Math.floor(Math.random()*(9)+1))*50);
-  var calcY = Math.abs((Math.floor(Math.random()*(9)+1))*50);
+function newGem() {
+  var calcX = Math.abs((Math.floor(Math.random() * (9) + 1)) * 50);
+  var calcY = Math.abs((Math.floor(Math.random() * (9) + 1)) * 50);
   var xGem = 0;
   var yGem = 0;
-    if(calcX < 505){xGem = Math.floor(calcX-25);
-    } else{xGem = calcX;}
-    if(calcY < 100){yGem = calcY + 75;
-    } else{yGem = Math.floor(calcY-25);}
+  if (calcX < 505) {
+    xGem = Math.floor(calcX - 25);
+  } else {
+    xGem = calcX;
+  }
+  if (calcY < 100) {
+    yGem = calcY + 75;
+  } else {
+    yGem = Math.floor(calcY - 25);
+  }
   var gemArray = ['images/Gem Blue.png', 'images/Gem Green.png', 'images/Gem Orange.png'];
-  var selectGem = Math.floor(Math.random()*(3));
+  var selectGem = Math.floor(Math.random() * (3));
   var createGem = gemArray[selectGem];
   var imgGem = createGem;
   allGems.push(new Gem(xGem, yGem, imgGem));
 }
 
-function createGems(){
-  if(allGems.length < 3){
-  var len = allGems.length;
- for(i=3; len<i; i--){
-   newGem();
- }
-}}
+function createGems() {
+  if (allGems.length < 3) {
+    var len = allGems.length;
+    for (i = 3; len < i; i--) {
+      newGem();
+    }
+  }
+}
 
 //Gem points
-Gem.prototype.points = function(){
-  if(this.sprite == 'images/Gem Blue.png'){
+Gem.prototype.points = function() {
+  if (this.sprite == 'images/Gem Blue.png') {
     gemBlueTotal += 25;
     $('h3.pointsBlue').replaceWith('<h3 class="pointsBlue">' + gemBlueTotal + '</h3>');
   }
-  if(this.sprite == 'images/Gem Green.png'){
+  if (this.sprite == 'images/Gem Green.png') {
     gemGreenTotal += 50;
     $('h3.pointsGreen').replaceWith('<h3 class="pointsGreen">' + gemGreenTotal + '</h3>');
   }
-  if(this.sprite == 'images/Gem Orange.png'){
+  if (this.sprite == 'images/Gem Orange.png') {
     $('h3.pointsOrange').replaceWith('<h3 class="pointsOrange">' + gemOrangeTotal + '</h3>');
     gemOrangeTotal += 75;
   }
 };
 
 //adding of point
-Player.prototype.points = function(){
+Player.prototype.points = function() {
   totalPoints = points + gemBlueTotal + gemGreenTotal + gemOrangeTotal;
   $('h3.points').replaceWith('<h3 class="points">' + totalPoints + '</h3>');
 };
 //restart - player to return to Point of Origin
-Player.prototype.restart = function(){
+Player.prototype.restart = function() {
   this.x = 200;
   this.y = 400;
 };
@@ -164,30 +174,32 @@ Player.prototype.restart = function(){
 // Instantiate objects.
 var allEnemies = [];
 
-var enemyCreation = (function createEnemies(){
+var enemyCreation = (function createEnemies() {
   allEnemies.push(new Enemy(100, 100));
   return createEnemies;
 }());
 
 //new Enemy to be created
-function newEnemy(){
-  var xEnemy= Math.abs((Math.floor(Math.random()*(40)-6))*10);
-  var yEnemy= Math.abs((Math.floor(Math.random()*(3)+1))*100);
-  if(allEnemies.length < 12){
-  allEnemies.push(new Enemy(xEnemy, yEnemy));
+function newEnemy() {
+  var xEnemy = Math.abs((Math.floor(Math.random() * (40) - 6)) * 10);
+  var yEnemy = Math.abs((Math.floor(Math.random() * (3) + 1)) * 100);
+  if (allEnemies.length < 12) {
+    allEnemies.push(new Enemy(xEnemy, yEnemy));
   }
 }
 
-var player = new Player(200,400);
+var player = new Player(200, 400);
 
 
 //create lives
 //5 lives
-var createLife = function(){for(i=1; i<=5; i++){
-$('ul.lives').append('<li><img class="heart" src="images/Heart.png"></li>');
-}};
+var createLife = function() {
+  for (i = 1; i <= 5; i++) {
+    $('ul.lives').append('<li><img class="heart" src="images/Heart.png"></li>');
+  }
+};
 //reset the game
-function gameReset(){
+function gameReset() {
   $('div.modal-content').empty();
   $('div.modal').hide();
   points = 0;
@@ -210,23 +222,25 @@ function gameReset(){
 
 //game over
 
-function blink(){
+function blink() {
   $('.blink').fadeOut(500).fadeIn(500);
 }
 
 setInterval(blink, 1000);
 
-Player.prototype.gameOver = function(){
+Player.prototype.gameOver = function() {
   $('div.modal').show();
   $('div.modal-content').append('<h1 class="blink">GAME OVER</h1>');
-  if(totalPoints > highscore){
+  if (totalPoints > highscore) {
     highscore = totalPoints;
-      $('div.modal-content').append('<h2>New High Score!!!</h2>');
-      $('div.modal-content').append('<h2>' + highscore + '</h2>');
-  } else {$('div.modal-content').append('<h2>Your Score</h2>');
-          $('div.modal-content').append('<h2>'+ totalPoints +'</h2>');
-          $('div.modal-content').append('<h2>Highest Score</h2>');
-          $('div.modal-content').append('<h2>' + highscore + '</h2>');}
+    $('div.modal-content').append('<h2>New High Score!!!</h2>');
+    $('div.modal-content').append('<h2>' + highscore + '</h2>');
+  } else {
+    $('div.modal-content').append('<h2>Your Score</h2>');
+    $('div.modal-content').append('<h2>' + totalPoints + '</h2>');
+    $('div.modal-content').append('<h2>Highest Score</h2>');
+    $('div.modal-content').append('<h2>' + highscore + '</h2>');
+  }
   $('div.modal-content').append('<button class="button01">Play Again?</h1>');
   $('.button01').click(gameReset);
 };
@@ -235,12 +249,12 @@ Player.prototype.gameOver = function(){
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
+  var allowedKeys = {
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down'
+  };
 
-    player.handleInput(allowedKeys[e.keyCode]);
+  player.handleInput(allowedKeys[e.keyCode]);
 });
